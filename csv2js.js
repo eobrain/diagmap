d0f0cb71403eb9cc81453cc1977d63050b410e13
Eamonn O'Brien-Strain <eobrain@gmail.com>
2019-08-02 22:30:49 -0700

    Use different CSV parsing library.

--- csv2js.js
+++ csv2js.js
@@ -1,15 +1,18 @@
-const Papa = require('papaparse')
+const csv = require('csv-parser')
 const fs = require('fs')
 
-const csv = fs.readFileSync('data.csv', 'utf8')
-const data = Papa.parse(csv)
+const q = (s) => s.includes("'") ? `"${s.trim()}"` : `'${s.trim()}'`
 
-const q = (s) => s.includes("'") ? `"${s}"` : `'${s}'`
-
-console.log(`window.diagmapData = [`)
-for (const line of data.data) {
-  if (line.length === 4 && line[3]) {
-    console.log(`  [${q(line[1])}, ${q(line[2])}, ${q(line[3])}],`)
-  }
-}
-console.log(`]`)
+let nextIsHeader = true
+console.log('window.diagmapData = [')
+fs.createReadStream('data.csv')
+  .pipe(csv(['_', 'g', 'd', 'c']))
+  .on('data', (data) => {
+    if (data.g && data.d && data.c) {
+      if (!nextIsHeader) {
+        console.log(`[${q(data.g)}, ${q(data.d)}, ${q(data.c)}],`)
+      }
+      nextIsHeader = false
+    }
+  })
+  .on('end', () => console.log(']'))
